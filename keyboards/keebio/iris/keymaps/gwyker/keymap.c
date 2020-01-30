e35a67a745bc1e614047f595319afd866be28140
Grayson Wyker <gwyker@yahoo.com>
2020-01-30 18:14:30 -0500

    encoders updated

--- keyboards/keebio/iris/keymaps/gwyker/keymap.c
+++ keyboards/keebio/iris/keymaps/gwyker/keymap.c
@@ -76,85 +76,112 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSLS, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS, LCTL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, TG(4), KC_LALT, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_BSPC, MO(1), KC_LGUI, MO(2), KC_SPC, KC_ENT),
-	[1] = LAYOUT(KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PGUP, RALT(KC_TAB), KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_PGDN, KC_DEL, KC_TRNS, KC_LEFT, KC_RGHT, KC_UP, KC_LBRC, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_PLUS, KC_HOME, KC_LALT, KC_TRNS, KC_TRNS, KC_TRNS, KC_DOWN, KC_LCBR, KC_LPRN, KC_RPRN, KC_RCBR, KC_P1, KC_P2, KC_P3, KC_MINS, KC_END, KC_TRNS, KC_TRNS, KC_TRNS, MO(3), KC_DEL, KC_P0),
-	[2] = LAYOUT(KC_F12, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_TRNS, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_UNDS, KC_EQL, KC_HOME, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LALT, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGDN, KC_MINS, KC_TRNS, KC_TRNS, KC_PLUS, KC_END, KC_TRNS, KC_TRNS, KC_TRNS, KC_RALT, MO(3), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET),
+	[1] = LAYOUT(KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PGUP, RALT(KC_TAB), KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_PGDN, KC_DEL, KC_TRNS, KC_LEFT, KC_RGHT, KC_UP, KC_LBRC, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_PLUS, KC_HOME, KC_LALT, KC_TRNS, KC_TRNS, KC_TRNS, KC_DOWN, KC_LCBR, KC_LPRN, KC_RPRN, KC_RCBR, KC_P1, KC_P2, KC_P3, KC_MINS, KC_END, KC_TRNS, KC_TRNS, KC_TRNS, MO(2), KC_DEL, KC_P0),
+	[2] = LAYOUT(KC_F12, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_TRNS, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_UNDS, KC_EQL, KC_HOME, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LALT, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGDN, KC_MINS, KC_TRNS, KC_TRNS, KC_PLUS, KC_END, KC_TRNS, KC_TRNS, KC_TRNS, KC_RALT, KC_TRNS, MO(1), KC_TRNS, KC_TRNS, KC_TRNS, RESET),
 	[3] = LAYOUT(KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_VOLU, KC_TRNS, KC_TRNS, RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, KC_MUTE, KC_MSTP, KC_MPLY, KC_VOLD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 	[4] = LAYOUT(KC_ENT, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSLS, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS, KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_TRNS, KC_DEL, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, MO(1), KC_SPC, KC_RALT, MO(2), KC_SPC, KC_ENT)
 };
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case QWERTY:
-      if (record->event.pressed) {
-        set_single_persistent_default_layer(_QWERTY);
-      }
-      return false;
-      break;
-    case LOWER:
-      if (record->event.pressed) {
-        layer_on(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _POWER);
-      } else {
-        layer_off(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _POWER);
-      }
-      return false;
-      break;
-    case RAISE:
-      if (record->event.pressed) {
-        layer_on(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _POWER);
-      } else {
-        layer_off(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _POWER);
-      }
-      return false;
-      break;
-    case POWER:
-      if (record->event.pressed) {
-        layer_on(_POWER);
-      } else {
-        layer_off(_POWER);
-      }
-      return false;
-      break;
-  }
-  return true;
-}
+// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+//   switch (keycode) {
+//     case QWERTY:
+//       if (record->event.pressed) {
+//         set_single_persistent_default_layer(_QWERTY);
+//       }
+//       return false;
+//       break;
+//     case LOWER:
+//       if (record->event.pressed) {
+//         layer_on(_LOWER);
+//         update_tri_layer(_LOWER, _RAISE, _POWER);
+//       } else {
+//         layer_off(_LOWER);
+//         update_tri_layer(_LOWER, _RAISE, _POWER);
+//       }
+//       return false;
+//       break;
+//     case RAISE:
+//       if (record->event.pressed) {
+//         layer_on(_RAISE);
+//         update_tri_layer(_LOWER, _RAISE, _POWER);
+//       } else {
+//         layer_off(_RAISE);
+//         update_tri_layer(_LOWER, _RAISE, _POWER);
+//       }
+//       return false;
+//       break;
+//     case POWER:
+//       if (record->event.pressed) {
+//         layer_on(_POWER);
+//       } else {
+//         layer_off(_POWER);
+//       }
+//       return false;
+//       break;
+//   }
+//   return true;
+// }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
+    state = update_tri_layer_state(state, _LOWER, _RAISE, _POWER);
     switch (get_highest_layer(state)) {
         case _LOWER:
             rgblight_sethsv_noeeprom(HSV_GREEN);
             break;
         case _RAISE:
-            rgblight_sethsv_noeeprom(HSV_GOLD);
+            rgblight_sethsv_noeeprom(HSV_AZURE);
             break;
         case _POWER:
             rgblight_sethsv_noeeprom(HSV_RED);
+            break;
+        case _GAME:
+            rgblight_sethsv_noeeprom(HSV_CYAN);
             break;
         default: //  for any other layers, or the default layer
             rgblight_sethsv_noeeprom(HSV_PURPLE);
             break;
     }
-  return state;
+    return state;
 }
 
 void encoder_update_user(uint8_t index, bool clockwise) {
     switch(biton32(layer_state)) {
+        case _LOWER:
+            if (!clockwise) {
+                tap_code(KC_VOLU);
+            } else {
+                tap_code(KC_VOLD);
+            }
+            break;
         case _RAISE:
-            if (clockwise) {
+            if (!clockwise) {
+                tap_code(LSFT(KC_TAB));
+            } else {
+                tap_code(LCTL(LSFT(KC_TAB)));
+            }
+            break;
+        default:
+            if (!clockwise) {
                 tap_code(KC_WH_D);
             } else {
                 tap_code(KC_WH_U);
             }
             break;
-        default:
-            if (clockwise) {
-                tap_code(KC_PGDN);
-            } else {
-                tap_code(KC_PGUP);
-            }
-            break;
     }
 }
+
+void keyboard_post_init_user(void) {
+  // Customise these values to desired behaviour
+  debug_enable=true;
+  debug_matrix=true;
+  debug_keyboard=true;
+  //debug_mouse=true;
+}
+
+// TODO
+// encoder on shift tab and shift ctl tab
+// swap backspace and ctrl/esc
+// tap for [] on alt or super or something
+// rethink arrow keys
+// mouse keys?
+// lower and raise on far thumb keys?
