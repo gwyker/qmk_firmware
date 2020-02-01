19c2acf69ae627cdb6b115ee69c6168f22b6de9d
Grayson Wyker <gwyker@yahoo.com>
2020-02-01 16:39:36 -0500

    big change, added RAISE symbols

--- keyboards/keebio/iris/keymaps/gwyker/keymap.c
+++ keyboards/keebio/iris/keymaps/gwyker/keymap.c
@@ -75,11 +75,11 @@ enum custom_keycodes {
 // };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[0] = LAYOUT(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSLS, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS, LCTL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, TG(4), KC_LALT, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_BSPC, MO(1), KC_LGUI, MO(2), KC_SPC, KC_ENT),
-	[1] = LAYOUT(KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PGUP, RALT(KC_TAB), KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_PGDN, KC_DEL, KC_TRNS, KC_LEFT, KC_RGHT, KC_UP, KC_LBRC, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_PLUS, KC_HOME, KC_LALT, KC_TRNS, KC_TRNS, KC_TRNS, KC_DOWN, KC_LCBR, KC_LPRN, KC_RPRN, KC_RCBR, KC_P1, KC_P2, KC_P3, KC_MINS, KC_END, KC_TRNS, KC_TRNS, KC_TRNS, MO(2), KC_DEL, KC_P0),
-	[2] = LAYOUT(KC_F12, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_TRNS, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_UNDS, KC_EQL, KC_HOME, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LALT, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGDN, KC_MINS, KC_TRNS, KC_TRNS, KC_PLUS, KC_END, KC_TRNS, KC_TRNS, KC_TRNS, KC_RALT, KC_TRNS, MO(1), KC_TRNS, KC_TRNS, KC_TRNS, RESET),
-	[3] = LAYOUT(KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_VOLU, KC_TRNS, KC_TRNS, RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, KC_MUTE, KC_MSTP, KC_MPLY, KC_VOLD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
-	[4] = LAYOUT(KC_ENT, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSLS, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS, KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_TRNS, KC_DEL, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, MO(1), KC_SPC, KC_RALT, MO(2), KC_SPC, KC_ENT)
+	[0] = LAYOUT(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSLS, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS, KC_LGUI, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSPO, KC_Z, KC_X, KC_C, KC_V, KC_B, TG(4), KC_CAPS, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSPC, LCTL_T(KC_ESC), LT(1,OSM(MOD_LALT)), KC_BSPC, KC_SPC, LT(2,KC_ENT), KC_RSFT),
+	[1] = LAYOUT(KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_TRNS, RALT(KC_TAB), KC_TRNS, KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_P7, KC_P8, KC_P9, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_P4, KC_P5, KC_P6, KC_HOME, KC_PGUP, KC_LALT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LPRN, KC_RPRN, KC_TRNS, KC_P1, KC_P2, KC_P3, KC_END, KC_PGDN, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL, KC_TRNS, KC_P0),
+	[2] = LAYOUT(KC_F12, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_TRNS, KC_LCBR, KC_LPRN, KC_LBRC, KC_EQL, KC_TILD, KC_PIPE, KC_PLUS, KC_RBRC, KC_RPRN, KC_RCBR, KC_TRNS, KC_TRNS, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DQUO, KC_LALT, KC_TRNS, KC_TRNS, KC_TRNS, KC_UNDS, KC_GRV, KC_MUTE, KC_TRNS, KC_BSLS, KC_MINS, KC_LT, KC_GT, KC_QUES, KC_RALT, KC_TRNS, KC_TRNS, KC_DEL, KC_TRNS, KC_TRNS, RESET),
+	[3] = LAYOUT(KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_VOLU, KC_TRNS, KC_TRNS, RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, KC_MUTE, KC_MSTP, KC_MPLY, KC_VOLD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+	[4] = LAYOUT(KC_ENT, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSLS, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS, KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_TRNS, KC_DEL, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_RALT, KC_SPC, MO(1), MO(2), KC_SPC, KC_ENT)
 };
 
 // bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -155,9 +155,15 @@ void encoder_update_user(uint8_t index, bool clockwise) {
             break;
         case _RAISE:
             if (!clockwise) {
-                tap_code(LSFT(KC_TAB));
+                register_code(KC_LCTL);
+                tap_code(KC_TAB);
+                unregister_code(KC_LCTL);
             } else {
-                tap_code(LCTL(LSFT(KC_TAB)));
+                register_code(KC_LSFT);
+                register_code(KC_LCTL);
+                tap_code(KC_TAB);
+                unregister_code(KC_LSFT);
+                unregister_code(KC_LCTL);
             }
             break;
         default:
@@ -172,17 +178,13 @@ void encoder_update_user(uint8_t index, bool clockwise) {
 
 void keyboard_post_init_user(void) {
   // Customise these values to desired behaviour
-  // debug_enable=true;
-  // debug_matrix=true;
-  // debug_keyboard=true;
+  debug_enable=true;
+  debug_matrix=true;
+  debug_keyboard=true;
   //debug_mouse=true;
 }
 
 // TODO
-// encoder on shift tab and shift ctl tab
-// swap backspace and ctrl/esc
-// tap for [] on alt or super or something
-// rethink arrow keys
 // mouse keys?
 // lower and raise on far thumb keys?
 // tap dance ;; to :?
