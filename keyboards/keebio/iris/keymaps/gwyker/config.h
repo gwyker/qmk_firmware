b5c7857324c797ae1212d409f873d88bff6ae717
Grayson Wyker <gwyker@yahoo.com>
2020-02-08 22:28:31 -0500

    before overhaul, with nice @r macro

--- keyboards/keebio/iris/keymaps/gwyker/config.h
+++ keyboards/keebio/iris/keymaps/gwyker/config.h
@@ -35,3 +35,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define ENCODERS_PAD_A { B2 }
 #define ENCODERS_PAD_B { B3 }
 #define ENCODER_RESOLUTION 4
+
+#define TAPPING_TERM 170
+#define PERMISSIVE_HOLD
+// #define IGNORE_MOD_TAP_INTERRUPT
--- keyboards/keebio/iris/keymaps/gwyker/keymap.c
+++ keyboards/keebio/iris/keymaps/gwyker/keymap.c
@@ -21,6 +21,7 @@ enum custom_keycodes {
   ALT_TAB,
 //   EMOJI,
 };
+
 #define KC_SALT      ALT_TAB
 #define KC_CTL_ESC   LCTL_T(KC_ESC)
 #define KC_CTL_ENT   LCTL_T(KC_ENT)
@@ -131,20 +132,51 @@ const uint32_t PROGMEM unicode_map[] = {
   [SKULL] = 0x1F480,
 };
 
+// Tap Dance
+typedef struct {
+  bool is_press_action;
+  int state;
+} tap;
+
+enum {
+  SINGLE_TAP = 1,
+  SINGLE_HOLD = 2,
+  DOUBLE_TAP = 3,
+  DOUBLE_HOLD = 4,
+  DOUBLE_SINGLE_TAP = 5, //send two single taps
+  TRIPLE_TAP = 6,
+  TRIPLE_HOLD = 7
+};
+
+enum {
+  TD_SCLN = 0,
+  X_SFT
+};
+
+int cur_dance (qk_tap_dance_state_t *state);
+
+//for the x tap dance. Put it here so it can be used in any keymap
+void x_finished (qk_tap_dance_state_t *state, void *user_data);
+void x_reset (qk_tap_dance_state_t *state, void *user_data);
+
+//Tap Dance Definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+  // key   :   SEMICOLON
+  // one tap: ;
+  // two taps: :
+  [TD_SCLN] = ACTION_TAP_DANCE_DOUBLE(KC_SCLN, KC_COLN)
+
+  // key   :   SHIFT
+  // one tap: (
+  // two taps: [
+  // three taps: {
+  // hold: Shift
+
+// Other declarations would go here, separated by commas, if you have them
+};
+
 // const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-// 	[0] = LAYOUT(ALT_TAB, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSLS, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,
-//   LCTL_T(KC_ENT), LCTL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSPO, KC_Z, KC_X, KC_C, KC_V, KC_B, TG(4), KC_LALT, KC_N, KC_M,
-//   KC_COMM, KC_DOT, KC_SLSH, KC_RSPC, KC_BSPC, MO(1), LGUI_T(KC_ENT), LCTL_T(KC_SPC), LT(2,KC_SPC), KC_ENT),
-// 	[1] = LAYOUT(KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_APP,
-// RALT(KC_TAB), KC_TRNS, KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_P7, KC_P8, KC_P9, KC_PSCR,
-// KC_INS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, TG(5), KC_P4, KC_P5, KC_P6, KC_HOME,
-// KC_PGUP, KC_LALT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE, KC_TRNS, OSL(6), KC_P1, KC_P2, KC_P3, KC_END, KC_PGDN, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL, KC_TRNS, KC_P0),
-// 	[2] = LAYOUT(KC_F12, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_TRNS, KC_LCBR, KC_LPRN, KC_LBRC, KC_EQL, KC_TILD, KC_PIPE, KC_PLUS, KC_RBRC, KC_RPRN, KC_RCBR, KC_TRNS, KC_TRNS, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DQUO, KC_LALT, KC_TRNS, KC_TRNS, KC_TRNS, KC_UNDS, KC_GRV, KC_TRNS, KC_TRNS, KC_BSLS, KC_MINS, KC_LT, KC_GT, KC_QUES, KC_RALT, KC_TRNS, KC_TRNS, KC_DEL, KC_TRNS, KC_TRNS, RESET),
-// 	[3] = LAYOUT(KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_VOLU, KC_TRNS, KC_TRNS, RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, KC_MUTE, KC_MSTP, KC_MPLY, KC_VOLD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
-// 	[4] = LAYOUT(KC_ENT, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSLS, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS, KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_TRNS, KC_DEL, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_RALT, KC_SPC, MO(1), MO(2), KC_SPC, KC_ENT),
-// 	[5] = LAYOUT(KC_TRNS, KC_TRNS, KC_ACL0, KC_ACL1, KC_ACL2, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_BTN1, KC_MS_U, KC_BTN3, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN2, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
-//     [6] = LAYOUT(
 // 	[_QWERTY] = LAYOUT_kc(
 //   //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
 //      SALT, 1  , 2  , 3  , 4  , 5  ,                6  , 7  , 8  , 9  , 0  ,BSLS,
@@ -238,21 +270,61 @@ const uint32_t PROGMEM unicode_map[] = {
 
 //// TODO WHEN KEYMAPPIN ////
 // KC_F13 -> ALT_TAB
+// KC_F14 -> LT(_EMOJI, OSM(_EMOJI))
+// KC_LSPO ->
+// KC_RSPO ->
+// [disabled] KC_SCLN -> TD(TD_SCLN)
 // add emoji layer 6
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[0] = LAYOUT(ALT_TAB, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSLS, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, LCTL_T(KC_ENT), LCTL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSPO, KC_Z, KC_X, KC_C, KC_V, KC_B, TG(4), KC_LALT, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSPC, KC_BSPC, MO(1), LGUI_T(KC_ENT), LCTL_T(KC_SPC), LT(2,KC_SPC), KC_ENT),
-	[1] = LAYOUT(KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_APP, RALT(KC_TAB), KC_TRNS, KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_P7, KC_P8, KC_P9, KC_PSCR, KC_INS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, TG(5), KC_P4, KC_P5, KC_P6, KC_HOME, KC_PGUP, KC_LALT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE, KC_TRNS, MO(6), KC_P1, KC_P2, KC_P3, KC_END, KC_PGDN, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL, KC_TRNS, KC_P0),
-	[2] = LAYOUT(KC_F12, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_TRNS, KC_LCBR, KC_LPRN, KC_LBRC, KC_EQL, KC_TILD, KC_PIPE, KC_PLUS, KC_RBRC, KC_RPRN, KC_RCBR, KC_TRNS, KC_TRNS, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DQUO, KC_LALT, KC_TRNS, KC_TRNS, KC_TRNS, KC_UNDS, KC_GRV, KC_TRNS, KC_TRNS, KC_BSLS, KC_MINS, KC_LT, KC_GT, KC_QUES, KC_RALT, KC_TRNS, KC_TRNS, KC_DEL, KC_TRNS, KC_TRNS, RESET),
-	[3] = LAYOUT(KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_VOLU, KC_TRNS, KC_TRNS, RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, KC_MUTE, KC_MSTP, KC_MPLY, KC_VOLD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
-	[4] = LAYOUT(KC_ENT, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSLS, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS, KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_TRNS, KC_DEL, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_RALT, KC_SPC, MO(1), MO(2), KC_SPC, KC_ENT),
-	[5] = LAYOUT(KC_TRNS, KC_TRNS, KC_ACL0, KC_ACL1, KC_ACL2, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_BTN1, KC_MS_U, KC_BTN3, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN2, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
+	[0] = LAYOUT(
+        ALT_TAB, KC_1, KC_2, KC_3, KC_4, KC_5,             KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSLS,
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,             KC_Y, KC_U, KC_I, KC_O, KC_P, LCTL_T(KC_ENT),
+        LCTL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G,             KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
+        KC_LSPO, KC_Z, KC_X, KC_C, KC_V, KC_B, TG(4),         KC_LALT, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSPC,
+                                KC_BSPC, MO(1), KC_LGUI,     LCTL_T(KC_SPC), LT(2,KC_SPC), KC_ENT
+    ),
+	[1] = LAYOUT(
+        KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,             KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_APP,
+        RALT(KC_TAB), KC_TRNS, KC_TRNS, KC_UP, KC_TRNS, KC_TRNS,             KC_TRNS, KC_P7, KC_P8, KC_P9, KC_PSCR, KC_INS,
+        KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS,             TG(5), KC_P4, KC_P5, KC_P6, KC_HOME, KC_PGUP,
+        KC_LALT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE,         KC_TRNS, MO(6), KC_P1, KC_P2, KC_P3, KC_END, KC_PGDN,
+                                KC_TRNS, KC_TRNS, KC_TRNS,     KC_DEL, KC_TRNS, KC_P0
+    ),
+	[2] = LAYOUT(
+        KC_F12, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,             KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
+        KC_TRNS, KC_LCBR, KC_LPRN, KC_LBRC, KC_EQL, KC_TILD,             KC_PIPE, KC_PLUS, KC_RBRC, KC_RPRN, KC_RCBR, KC_TRNS,
+        KC_TRNS, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,             KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DQUO,
+        KC_LALT, KC_TRNS, KC_TRNS, KC_TRNS, KC_UNDS, KC_GRV, KC_TRNS,         KC_TRNS, KC_BSLS, KC_MINS, KC_LT, KC_GT, KC_QUES, KC_RALT,
+                                KC_TRNS, KC_TRNS, KC_DEL,     KC_TRNS, KC_TRNS, RESET
+    ),
+	[3] = LAYOUT(
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_TRNS,
+        KC_TRNS, KC_MPRV, KC_MNXT, KC_VOLU, KC_TRNS, KC_TRNS,             RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI,
+        KC_MUTE, KC_MSTP, KC_MPLY, KC_VOLD, KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS, KC_TRNS, RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD,
+                                KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS
+    ),
+	[4] = LAYOUT(
+        KC_ENT, KC_1, KC_2, KC_3, KC_4, KC_5,             KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSLS,
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,             KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS,
+        KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G,             KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_TRNS,         KC_DEL, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+                                KC_RALT, KC_SPC, MO(1),     MO(2), KC_SPC, KC_ENT
+    ),
+	[5] = LAYOUT(
+        KC_TRNS, KC_TRNS, KC_ACL0, KC_ACL1, KC_ACL2, KC_TRNS,             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_TRNS, KC_TRNS, KC_BTN1, KC_MS_U, KC_BTN3, KC_TRNS,             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,             KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN2, KC_TRNS, KC_TRNS, KC_NO,         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                                KC_NO, KC_NO, KC_NO,     KC_NO, KC_NO, KC_NO
+    ),
     [_EMOJI] = LAYOUT(
-        X(HART2), X(CRY2),X(WEARY),X(EYERT),X(SMIRK), X(TJOY),X(RECYC),X(UNAMU),X(MUSIC),X(OKHND),X(PENSV), X(PHEW),
-        X(THMUP), X(PRAY),X(SMILE),X(SMIL2),X(FLUSH), X(GRIN),X(HEART),  X(BYE), X(KISS),X(CELEB), X(COOL),X(NOEVS),
-        X(THMDN),X(SLEEP), X(CLAP),  X(CRY),  X(VIC),X(BHART),  X(SUN),X(SMEYE), X(WINK), X(MOON),X(CONFU),X(NOEVH),
-        X(POO), X(EYES), X(HUNRD),_______, X(SKULL),X(HORNS), X(HALO), X(FEAR),_______,X(YUMMY),X(DISAP),X(NOEVK),
-        _______, _______, _______, _______, _______, _______, _______, _______
+        X(HART2), X(CRY2), X(WEARY), X(EYERT), X(SMIRK), X(TJOY),             X(RECYC), X(UNAMU), X(MUSIC), X(OKHND), X(PENSV), X(PHEW),
+        X(THMUP), X(PRAY), X(SMILE), X(SMIL2), X(FLUSH), X(GRIN),             X(HEART), X(BYE), X(KISS), X(CELEB), X(COOL), X(NOEVS),
+        X(THMDN), X(SLEEP), X(CLAP), X(CRY), X(VIC), X(BHART),             X(SUN), X(SMEYE), X(WINK), X(MOON), X(CONFU), X(NOEVH),
+        X(POO), X(EYES), X(HUNRD), _______, X(SKULL), X(HORNS), X(HALO),         X(FEAR), _______, X(YUMMY), X(DISAP), X(NOEVK), _______, _______,
+                                _______, _______, _______,     _______, _______, _______
     ),
 };
 
@@ -314,11 +386,39 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
+// layer_state_t layer_state_set_user(layer_state_t state) {
+//     state = update_tri_layer_state(state, _LOWER, _RAISE, _POWER);
+//     switch (get_highest_layer(state)) {
+//         case _LOWER:
+//             rgblight_sethsv_noeeprom(HSV_GREEN);
+//             break;
+//         case _RAISE:
+//             rgblight_sethsv_noeeprom(HSV_AZURE);
+//             break;
+//         case _POWER:
+//             rgblight_sethsv_noeeprom(HSV_RED);
+//             break;
+//         case _GAME:
+//             rgblight_sethsv_noeeprom(HSV_CYAN);
+//             break;
+//         case _MOUSE:
+//             rgblight_sethsv_noeeprom(HSV_YELLOW);
+//             break;
+//         case _EMOJI:
+//             rgblight_sethsv_noeeprom(HSV_ORANGE);
+//             break;
+//         default: //  for any other layers, or the default layer
+//             rgblight_sethsv_noeeprom(HSV_PURPLE);
+//             break;
+//     }
+//     return state;
+// }
+
 layer_state_t layer_state_set_user(layer_state_t state) {
     state = update_tri_layer_state(state, _LOWER, _RAISE, _POWER);
     switch (get_highest_layer(state)) {
         case _LOWER:
-            rgblight_sethsv_noeeprom(HSV_GREEN);
+            rgblight_sethsv_at(HSV_GREEN);
             break;
         case _RAISE:
             rgblight_sethsv_noeeprom(HSV_AZURE);
@@ -328,6 +428,12 @@ layer_state_t layer_state_set_user(layer_state_t state) {
             break;
         case _GAME:
             rgblight_sethsv_noeeprom(HSV_CYAN);
+            break;
+        case _MOUSE:
+            rgblight_sethsv_noeeprom(HSV_YELLOW);
+            break;
+        case _EMOJI:
+            rgblight_sethsv_noeeprom(HSV_ORANGE);
             break;
         default: //  for any other layers, or the default layer
             rgblight_sethsv_noeeprom(HSV_PURPLE);
@@ -386,6 +492,116 @@ void matrix_scan_user(void) {
     }
   }
 }
+
+/* Return an integer that corresponds to what kind of tap dance should be executed.
+ *
+ * How to figure out tap dance state: interrupted and pressed.
+ *
+ * Interrupted: If the state of a dance dance is "interrupted", that means that another key has been hit
+ *  under the tapping term. This is typically indicitive that you are trying to "tap" the key.
+ *
+ * Pressed: Whether or not the key is still being pressed. If this value is true, that means the tapping term
+ *  has ended, but the key is still being pressed down. This generally means the key is being "held".
+ *
+ * One thing that is currenlty not possible with qmk software in regards to tap dance is to mimic the "permissive hold"
+ *  feature. In general, advanced tap dances do not work well if they are used with commonly typed letters.
+ *  For example "A". Tap dances are best used on non-letter keys that are not hit while typing letters.
+ *
+ * Good places to put an advanced tap dance:
+ *  z,q,x,j,k,v,b, any function key, home/end, comma, semi-colon
+ *
+ * Criteria for "good placement" of a tap dance key:
+ *  Not a key that is hit frequently in a sentence
+ *  Not a key that is used frequently to double tap, for example 'tab' is often double tapped in a terminal, or
+ *    in a web form. So 'tab' would be a poor choice for a tap dance.
+ *  Letters used in common words as a double. For example 'p' in 'pepper'. If a tap dance function existed on the
+ *    letter 'p', the word 'pepper' would be quite frustating to type.
+ *
+ * For the third point, there does exist the 'DOUBLE_SINGLE_TAP', however this is not fully tested
+ *
+ */
+int cur_dance (qk_tap_dance_state_t *state) {
+  if (state->count == 1) {
+    if (state->interrupted || !state->pressed)  return SINGLE_TAP;
+    //key has not been interrupted, but they key is still held. Means you want to send a 'HOLD'.
+    else return SINGLE_HOLD;
+  }
+  else if (state->count == 2) {
+    /*
+     * DOUBLE_SINGLE_TAP is to distinguish between typing "pepper", and actually wanting a double tap
+     * action when hitting 'pp'. Suggested use case for this return value is when you want to send two
+     * keystrokes of the key, and not the 'double tap' action/macro.
+    */
+    if (state->interrupted) return DOUBLE_SINGLE_TAP;
+    else if (state->pressed) return DOUBLE_HOLD;
+    else return DOUBLE_TAP;
+  }
+  //Assumes no one is trying to type the same letter three times (at least not quickly).
+  //If your tap dance key is 'KC_W', and you want to type "www." quickly - then you will need to add
+  //an exception here to return a 'TRIPLE_SINGLE_TAP', and define that enum just like 'DOUBLE_SINGLE_TAP'
+  if (state->count == 3) {
+    if (state->interrupted || !state->pressed)  return TRIPLE_TAP;
+    else return TRIPLE_HOLD;
+  }
+  else return 8; //magic number. At some point this method will expand to work for more presses
+}
+
+//instanalize an instance of 'tap' for the 'x' tap dance.
+static tap xtap_state = {
+  .is_press_action = true,
+  .state = 0
+};
+
+void x_LSFT_finished (qk_tap_dance_state_t *state, void *user_data) {
+  xtap_state.state = cur_dance(state);
+  switch (xtap_state.state) {
+    case SINGLE_TAP: register_code(KC_LPRN); break;
+    case SINGLE_HOLD: register_code(KC_LSFT); break;
+    case DOUBLE_TAP: register_code(KC_LBRC); break;
+    case DOUBLE_HOLD: break;
+    //Last case is for fast typing. Assuming your key is `f`:
+    //For example, when typing the word `buffer`, and you want to make sure that you send `ff` and not `Esc`.
+    //In order to type `ff` when typing fast, the next character will have to be hit within the `TAPPING_TERM`, which by default is 200ms.
+  }
+}
+
+void x_LSFT_reset (qk_tap_dance_state_t *state, void *user_data) {
+  switch (xtap_state.state) {
+    case SINGLE_TAP: unregister_code(KC_LPRN); break;
+    case SINGLE_HOLD: unregister_code(KC_LSFT); break;
+    case DOUBLE_TAP: unregister_code(KC_LBRC); break;
+    case DOUBLE_HOLD: break;
+  }
+  xtap_state.state = 0;
+}
+
+void x_RSFT_finished (qk_tap_dance_state_t *state, void *user_data) {
+  xtap_state.state = cur_dance(state);
+  switch (xtap_state.state) {
+    case SINGLE_TAP: register_code(KC_RPRN); break;
+    case SINGLE_HOLD: register_code(KC_RSFT); break;
+    case DOUBLE_TAP: register_code(KC_RBRC); break;
+    case DOUBLE_HOLD: break;
+    //Last case is for fast typing. Assuming your key is `f`:
+    //For example, when typing the word `buffer`, and you want to make sure that you send `ff` and not `Esc`.
+    //In order to type `ff` when typing fast, the next character will have to be hit within the `TAPPING_TERM`, which by default is 200ms.
+  }
+}
+
+void x_RSFT_reset (qk_tap_dance_state_t *state, void *user_data) {
+  switch (xtap_state.state) {
+    case SINGLE_TAP: unregister_code(KC_RPRN); break;
+    case SINGLE_HOLD: unregister_code(KC_RSFT); break;
+    case DOUBLE_TAP: unregister_code(KC_RBRC); break;
+    case DOUBLE_HOLD: break;
+  }
+  xtap_state.state = 0;
+}
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [X_LSFT]     = ACTION_TAP_DANCE_FN_ADVANCED(NULL,x_LSFT_finished, x_LSFT_reset)
+  [X_RSFT]     = ACTION_TAP_DANCE_FN_ADVANCED(NULL,x_RSFT_finished, x_RSFT_reset)
+};
 
 // TODO
 // emoji layer for linux
